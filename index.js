e8f03de68893bbd0554fd0fbe708ec419050f490
掉链子了咯 <74001180+tanqihua@users.noreply.github.com>
2024-01-10 16:30:58 +0800

    pushh

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ admin.initializeApp({
   storageBucket: "testerdemo-888a3.appspot.com",
 });
 
-// Get a reference to the storage bucket
-const bucket = admin.storage().bucket();
+const storage = getStorage();
 
 app.get("/", async (req, res) => {
   const { url } = req.query;
@@ -37,43 +36,31 @@ app.get("/", async (req, res) => {
     url ??
     "https://firebasestorage.googleapis.com/v0/b/testerdemo-888a3.appspot.com/o/cktest-sg%2Fd7cc432c1fa7470d8a818c74466ce548.png?alt=media&token=6dd322ea-19e0-4583-855c-d31223aff413";
 
-  await createImage(_urll, (imageBuffer) => {
-    try {
-      const bucket = getStorage().bucket();
-      const file = bucket.file("cktest-sg/" + Date.now() + ".png");
-      const options = {
-        metadata: {
-          contentType: "image/png", // Set the content type appropriately for a PNG image
-        },
-      };
+  const imageBuffer = await createImage(_urll);
 
-      // Upload the buffer to Firebase Storage
-      file.save(imageBuffer, options, (err) => {
-        if (err) {
-          res.send({
-            status: "error",
-            message: err.message,
-          });
-        }
+  const bucket = getStorage().bucket();
 
-        // get public url
-        file
-          .getSignedUrl({
-            action: "read",
-            expires: "03-09-2491",
-          })
-          .then((url) => {
-            res.send({
-              status: "ok",
-              url: url[0],
-            });
-          });
-      });
-    } catch (e) {
-      res.send({
-        status: "error",
-        message: e.message,
-      });
+  const file = bucket.file("ck/" + Date.now() + ".png");
+  const options = {
+    metadata: {
+      contentType: "image/png", // Set the content type appropriately for a PNG image
+    },
+  };
+
+  file.save(imageBuffer, options, function (err) {
+    if (err) {
+      res.send("not ok");
+      return;
     }
+
+    // get public url
+    file
+      .getSignedUrl({
+        action: "read",
+        expires: "03-09-2491",
+      })
+      .then((url) => {
+        res.send(url[0]);
+      });
   });
 });
--- utils.js
+++ utils.js
@@ -6,62 +6,9 @@ const Replicate = require("replicate");
 const masking = new Canvas.Image();
 masking.src = fs.readFileSync("./images/masking.png");
 
-async function createImage(url, callBack = () => {}) {
-  // Usage
-  let _url = await ReplicateAIP(url);
-  try {
-    getImageBuffer(_url)
-      .then((buffer) => {
-        const img = new Canvas.Image();
-
-        img.src = buffer;
-
-        // before
-        const canvas = Canvas.createCanvas(1920, 1080);
-        const ctx = canvas.getContext("2d");
-
-        ctx.drawImage(img, 0, 0, 1920, 1080);
-
-        // masking
-        ctx.drawImage(masking, 0, 0, 1920, 1080);
-
-        // get all pixel data
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const data = imageData.data;
-
-        // change pixel data
-        for (let i = 0; i < data.length; i += 4) {
-          // get rgb
-          const r = data[i];
-          const g = data[i + 1];
-          const b = data[i + 2];
-
-          if (g >= 100 && r < 100 && b < 100) {
-            data[i] = 0;
-            data[i + 1] = 0;
-            data[i + 2] = 0;
-            data[i + 3] = 0;
-          }
-        }
-        // put data back
-        ctx.putImageData(imageData, 0, 0);
-
-        const imageBuffer = canvas.toBuffer("image/png", { quality: 0.8 });
-
-        callBack(imageBuffer);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-// let imgg = createImage(
-//   "https://storage.googleapis.com/replicate-files/TZsTL6ghqF5iJVVErnO3OhlrPeiKfwvSdGbcReTTDYvGiL6jA/6c60e956-faad-470e-98c4-8d387191af3c.jpg"
-// );
-// fs.writeFileSync("./images/test.png", imgg);
+const replicate = new Replicate({
+  auth: "r8_ErBf6Pi4cMtEMqkvThTB6dQHikGUKSU4SbJV9",
+});
 
 function getImageBuffer(url) {
   return new Promise((resolve, reject) => {
@@ -81,26 +28,68 @@ function getImageBuffer(url) {
   });
 }
 
-const replicate = new Replicate({
-  auth: "r8_ErBf6Pi4cMtEMqkvThTB6dQHikGUKSU4SbJV9",
-});
-
 const ReplicateAIP = async (target) => {
   const output = await replicate.run(
-    "yan-ops/face_swap:a7d6a0118f021279b8966473f302b1d982fd3920426ebd334e8f64d5caf84418",
+    "omniedgeio/face-swap:c2d783366e8d32e6e82c40682fab6b4c23b9c6eff2692c0cf7585fc16c238cfe",
     {
       input: {
-        det_thresh: 0.1,
-        request_id: "",
-        target_image:
-          "https://firebasestorage.googleapis.com/v0/b/testerdemo-888a3.appspot.com/o/cktest-sg%2Fmodel_transparent.png?alt=media&token=48f2e9fd-6852-4850-b1ac-9574685b83e8",
-        source_image: target,
+        swap_image: target,
+        target_image: "https://i.ibb.co/55CBpQS/model-transparent.png",
       },
     }
   );
 
-  return output.image;
+  return output;
 };
+
+async function createImage(url) {
+  // Usage
+  let _url = await ReplicateAIP(url);
+  console.log(_url);
+  let buffer = await getImageBuffer(_url);
+  const img = new Canvas.Image();
+  img.src = buffer;
+
+  // before
+  const canvas = Canvas.createCanvas(1920, 1080);
+  const ctx = canvas.getContext("2d");
+
+  ctx.drawImage(img, 0, 0, 1920, 1080);
+
+  // masking
+  ctx.drawImage(masking, 0, 0, 1920, 1080);
+
+  // get all pixel data
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+
+  // change pixel data
+  for (let i = 0; i < data.length; i += 4) {
+    // get rgb
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+
+    if (g >= 100 && r < 100 && b < 100) {
+      data[i] = 0;
+      data[i + 1] = 0;
+      data[i + 2] = 0;
+      data[i + 3] = 0;
+    }
+  }
+  // put data back
+  ctx.putImageData(imageData, 0, 0);
+
+  const imageBuffer = canvas.toBuffer("image/png", { quality: 0.8 });
+
+  return imageBuffer;
+}
+
+// async function main() {
+//   let img = await createImage(
+//     "https://storage.googleapis.com/replicate-files/TZsTL6ghqF5iJVVErnO3OhlrPeiKfwvSdGbcReTTDYvGiL6jA/6c60e956-faad-470e-98c4-8d387191af3c.jpg"
+//   );
+// }
 
 module.exports = {
   createImage,
